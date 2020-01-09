0c146a4278d0732677028f019ec24bec26788334
Romell Pineda <romell.pineda@gmail.com>
2020-01-08 20:53:13 -0800

    recursive contains target

--- code-challenges/array-binary-search/BinarySearch.java
+++ code-challenges/array-binary-search/BinarySearch.java
@@ -2,13 +2,13 @@ public class BinarySearch {
   public static void main (String[] args) {
     System.out.println("Code Challenge 3: Binary Search Array");
 
-    int[] testArray = {1, 2, 3, 4, 5, 6, 7};
-    int yld = binarySearch(testArray, 3);
+    int[] testArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int yld = binarySearch(testArray, 8);
     System.out.println(yld);
   }
 
   public static int binarySearch(int[] arr, int target) {
-    if (arr.length < 1 || target < arr[0] || target > arr[arr.length - 1]) {
+    if (arr.length <= 1 || target < arr[0] || target > arr[arr.length - 1]) {
       return -1;
     }
 
@@ -18,34 +18,32 @@ public class BinarySearch {
     while(rightBorder > leftBorder) {
       int mid = (rightBorder - leftBorder) / 2;
       if (arr[mid] == target) {
-        return mid;
+        return 100;
       }
       if (target < mid) {
         rightBorder = mid;
-        binarySearch(mySlice(arr, leftBorder, rightBorder), target);
-        // System.out.print(mySlice(arr, leftBorder, rightBorder));
-        // return -99;
+        return binarySearch(mySlice(arr, leftBorder, rightBorder), target);
       }
       if (target > mid) {
         leftBorder = mid;
-        binarySearch(mySlice(arr, leftBorder, rightBorder), target);
-        // System.out.println(mySlice(arr, leftBorder, rightBorder));
-        // return 99;
+        return binarySearch(mySlice(arr, leftBorder, rightBorder), target);
       }
-      // System.out.println(leftBorder);
-      // System.out.println(rightBorder);
-      // leftBorder++;
     }
-    return -33;
+
+    return -99;
   }
 
   public static int[] mySlice(int[] arr, int start, int end) {
+    if(arr.length == 1) {
+      return arr;
+    }
+
     int distance = end - start;
     int[] slicedArray = new int[distance + 1];
+
     for(int i = 0; i <= distance; i++) {
       slicedArray[i] = arr[start + i];
     }
-  
     return slicedArray;
   }
 }
