281c067f00cda773a8564b0fe5ea062bb61ef056
Romell Pineda <romell.pineda@gmail.com>
2020-01-27 12:36:22 -0800

    TreeNode test

---
+++ code401challenges/src/main/java/code401challenges/tree/BinaryTree.java
@@ -0,0 +1,14 @@
+package code401challenges.tree;
+
+import java.util.ArrayList;
+
+public class BinaryTree {
+    TreeNode root;
+
+    public BinaryTree() {}
+
+    public BinaryTree(TreeNode treeNode) {
+        root = treeNode;
+    }
+
+}
---
+++ code401challenges/src/test/java/code401challenges/tree/TreeNodeTest.java
@@ -0,0 +1,23 @@
+package code401challenges.tree;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TreeNodeTest {
+
+    @Test
+    public void testTreeNodeInstance() {
+        TreeNode newTreeNode = new TreeNode(8);
+        assertEquals("should return correct TreeNode value", 8, newTreeNode.value);
+    }
+
+    @Test public void testTreeNodeQuickConstructor() {
+        TreeNode lTreeNode = new TreeNode(5);
+        TreeNode rTreeNode = new TreeNode(99);
+        TreeNode root = new TreeNode(42, lTreeNode, rTreeNode);
+
+        assertEquals("should return correct left TreeNode value", 5, root.left.value);
+        assertEquals("should return correct right TreeNode value", 99, root.right.value);
+    }
+}
