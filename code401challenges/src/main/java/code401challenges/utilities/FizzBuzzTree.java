30314114bedb93c1a7198c671984ebe853464cae
Romell Pineda <romell.pineda@gmail.com>
2020-01-29 11:03:47 -0800

    additional refactor

--- code401challenges/src/main/java/code401challenges/utilities/FizzBuzzTree.java
+++ code401challenges/src/main/java/code401challenges/utilities/FizzBuzzTree.java
@@ -1,3 +1,12 @@
+package code401challenges.utilities;
+
+import code401challenges.tree.BinaryTree;
+
 public class FizzBuzzTree {
+
+    public BinaryTree<String> fizzBuzz(BinaryTree<String> stringTree) {
+        BinaryTree<String> fbTree = new BinaryTree<>();
+        return fbTree;
+    }
 
 }
