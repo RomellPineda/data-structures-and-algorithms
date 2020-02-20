5b2d0e8b396088363a7ff3d1c6ec9f8a479467c6
Romell Pineda <romell.pineda@gmail.com>
2020-02-20 12:09:08 -0800

    class and test

---
+++ code401challenges/src/main/java/code401challenges/TreeIntersection/TreeIntersection.java
@@ -0,0 +1,16 @@
+package code401challenges.TreeIntersection;
+
+import code401challenges.tree.BinaryTree;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TreeIntersection {
+
+    public static ArrayList<Integer> treeIntersection(BinaryTree<Integer> treeOne, BinaryTree<Integer> treeTwo) {
+        ArrayList<Integer> outputArray = new ArrayList<>();
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        // coverage for edge repeat value in same outputArray
+        return outputArray;
+    }
+}
--- code401challenges/src/test/java/code401challenges/hashtable/HashtableTest.java
+++ code401challenges/src/test/java/code401challenges/hashtable/HashtableTest.java
@@ -10,6 +10,12 @@ public class HashtableTest {
     public void testHashtable() {
         Hashtable<String, Integer> smashTable = new Hashtable<>();
         smashTable.add("pancake", 42);
-        System.out.println(smashTable.get("pancake"));
+        smashTable.add("pineapple", 99);
+        smashTable.add("frenchFry", 6);
+        smashTable.add("coconuts", 101);
+        smashTable.add("waffles", 13);
+        smashTable.add("sake", 56);
+        int output = smashTable.get("coconuts");
+        assertEquals("should return correct value associated with key", 101, output);
     }
 }
